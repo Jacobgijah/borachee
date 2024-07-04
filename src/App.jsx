9e64644991ccdff94074f695ea3079e24e8554bc
Jacobgijah <jacobgijjah@gmail.com>
2024-07-04 17:11:23 +0300

    add spinning load

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Products from './Components/Products/Products'
-import Title from './Components/Title/Title'
-import About from './Components/About/About'
-// import Campus from './Components/Campus/Campus'
-import Testimonials from './Components/Testimonials/Testimonials'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Products from './Components/Products/Products';
+import Title from './Components/Title/Title';
+import About from './Components/About/About';
+// import Campus from './Components/Campus/Campus';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import { TailSpin } from 'react-loader-spinner';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay to show the loading spinner
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      <Navbar/>
-      <Hero />
-      <div className="container">
-        <Title subTitle='OUR PRODUCTS' title='What We Offer' />
-        <Products />
-        <About />
-        {/* <Title subTitle='GALLERY' title='Campus Photos' />
-        <Campus /> */}
-        <Title subTitle='PARTNERS' title='What Partners Says' />
-        <Testimonials />
-        <Title subTitle='CONTACT US' title='Get In Touch' />
-        <Contact />
-        <Footer />
-      </div>
+      {loading ? (
+        <div className="loading-spinner">
+          <TailSpin
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <div className="container">
+            <Title subTitle="OUR PRODUCTS" title="What We Offer" />
+            <Products />
+            <About />
+            <Title subTitle="PARTNERS" title="What Partners Say" />
+            <Testimonials />
+            <Title subTitle="CONTACT US" title="Get In Touch" />
+            <Contact />
+            <Footer />
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
