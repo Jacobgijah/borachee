07e7b4fe44694ffac133fa47e810151d0279768f
Jacobgijah <jacobgijjah@gmail.com>
2024-07-15 23:06:44 +0300

    updates on progress_2

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -9,16 +9,16 @@ const Brands = () => {
   return (
     <div className='brand-logos'>
       <div className='brand-logo'>
-        <img src={brand_1} alt="Brand 1" />
-        <span>EasyFix</span>
-      </div>
-      <div className='brand-logo'>
         <img src={brand_2} alt="Brand 2" />
         <span>EuroCut</span>
       </div>
       <div className='brand-logo'>
         <img src={brand_3} alt="Brand 3" />
         <span>SALi</span>
+      </div>
+      <div className='brand-logo'>
+        <img src={brand_1} alt="Brand 1" />
+        <span>EasyFix</span>
       </div>
     </div>
   );
